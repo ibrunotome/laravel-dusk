5e382fdd6e92b68c6445d4b2ac2561bd09601cab
Bruno Tomé <ibrunotome@gmail.com>
2018-02-20 22:57:00 -0300

    Criando o primeiro teste

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     *
+     * @throws \Exception
+     * @throws \Throwable
+     */
+    public function testLogin()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'teste@teste.com'
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', 'secret')
+                ->press('Login')
+                ->assertPathIs('/home');
+        });
+    }
+}
